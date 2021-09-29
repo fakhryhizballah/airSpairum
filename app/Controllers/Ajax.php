cf9715503570a3537e7493ba358b50b0336fe1f0
fakhryhizballah <falehry88@gmail.com>
2021-09-29 09:12:30 +0700

    Belum Kelar baru

---
+++ app/Controllers/Ajax.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Libraries\AuthLibaries;
+use App\Models\StasiunModel;
+use App\Models\UserModel;
+use App\Models\OtpModel;
+use CodeIgniter\I18n\Time;
+use Generator;
+
+class Ajax extends BaseController
+{
+    public function __construct()
+    {
+        $this->AuthLibaries = new AuthLibaries();
+        helper('cookie');
+        $this->StasiunModel = new StasiunModel();
+        $this->UserModel = new UserModel();
+        $this->OtpModel = new OtpModel();
+    }
+    public function index()
+    {
+        $akun = $this->AuthLibaries->authCek();
+        $take = $this->request->getVar('myRange');
+        // $take = ('22');
+        $id = $this->request->getVar('code');
+        // $id = ('eyJpZCI6IjAwMSIsInN0YXNpdW4iOiJSRUJBTC1ERU5QQVNBUiIsImlkX21lc2luIjoiUHJvQmFsaSIsIm5ld19pZCI6IlByb0JhbGkzIn0=');
+        $id_encode = base64_decode($id);
+        $id_mesin = (json_decode($id_encode, true));
+        $mesin = $this->StasiunModel->cek_newID($id_mesin['new_id']);
+        // $mesin = $this->StasiunModel->cek_newID('ProBali2');
+        // echo json_encode($id_mesin);
+        if (empty($mesin)) {
+            $data = [
+                'status' => '204',
+                'total' => $mesin['harga'] * ($take / 10),
+            ];
+            echo json_encode($data);
+        }
+        $sisaSaldo = $akun['debit'] - (($take / 10) * $mesin['harga']);
+        // echo json_encode($mesin);
+        if ($sisaSaldo >= '0') {
+            // dd($akun['debit']);
+
+            $data = [
+                'status' => '200',
+                'nama' => $mesin['nama'],
+                'harga' => $mesin['harga'],
+                'diambil' =>  $take * 10,
+                // 'total' => $mesin['harga'] * ($take / 10),
+                'total' => (($take) * $mesin['harga']),
+                'newID' => $id_mesin['new_id'],
+                'index' => $mesin['faktor'],
+                'sisaSaldo' => $sisaSaldo
+            ];
+            echo json_encode($data);
+        } else {
+            $data = [
+                'status' => '204',
+                'total' => $mesin['harga'] * ($take / 10),
+            ];
+            echo json_encode($data);
+        };
+        // echo json_encode("sisaSaldo");
+        // echo json_encode($data);
+    }
+    public function PushAir()
+    {
+        $akun = $this->AuthLibaries->authCek();
+        $data = $this->request->getVar();
+        $vaule = $data['diambil'];
+        $idMesin =  $data['newID'];
+
+        $server   = 'ws.spairum.my.id';
+        $port     = 1883;
+        $clientId =  $akun['id_user'];
+        $idMesin =  $idMesin;
+        $vaule =  $data['diambil'];
+        $nilai = $vaule / $data['index'];
+
+        // $id = $this->request->getVar('id');
+        $respoun = [
+            'id' => $idMesin,
+            'akun' => $akun['id_user'],
+            'vaule' =>   $nilai,
+            'HargaTotal' => $data['total']
+        ];
+        $myJSON = json_encode($respoun);
+        $connectionSettings = (new \PhpMqtt\Client\ConnectionSettings)
+            ->setUsername('spairum')
+            ->setPassword('broker');
+
+        $mqtt = new \PhpMqtt\Client\MqttClient($server, $port, $clientId);
+        $mqtt->connect($connectionSettings, true);
+        $mqtt->publish("start/$idMesin",  $myJSON);
+        $mqtt->disconnect();
+    }
+    public function Generator()
+    {
+        $db      = \Config\Database::connect();
+        $builder = $db->table('user');
+        $builder->select('*');
+        $builder->join('otp', 'otp.id_user = user.id_user');
+        $arr = $builder->get()->getResultObject();
+        // dd($arr);
+        $saldo = $db->table('saldo');
+        $history = $db->table('history');
+       
+            echo "$value->nama <br>";
+            echo "$value->id_user<br>";
+        }
+        // dd($query[0]->nama);
+    }
+}
