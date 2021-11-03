47cf0ed58121b8404aaa84937fe7a27eda64ea52
fakhryhizballah <falehry88@gmail.com>
2021-11-03 13:24:08 +0700

    set PWA

--- public/js/main.js
+++ public/js/main.js
@@ -347,12 +347,12 @@ $(window).on('resize', function() {
 
 // PWA 
 
-// if ('serviceWorker' in navigator) {
-//     navigator.serviceWorker.register('/sw.js')
-//         .then(function(registration) {
-//             console.log('Registration successful, scope is:', registration.scope);
-//         })
-//         .catch(function(error) {
-//             console.log('Service worker registration failed, error:', error);
-//         });
-// }
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js')
+        .then(function(registration) {
+            console.log('Registration successful, scope is:', registration.scope);
+        })
+        .catch(function(error) {
+            console.log('Service worker registration failed, error:', error);
+        });
+}
