e2ffe8ad827f81a4ca3d08559f53218531553c09
Syahmi33github <Syahmi33github@student.untan.ac.id>
2021-12-04 10:49:36 +0700

    bang logo same warna harga udh dipisah berdasarkan jenis status, same atasny saya ubah dikit

--- app/Views/user/riwayat.php
+++ app/Views/user/riwayat.php
@@ -4,7 +4,15 @@
 <!-- page content here -->
 
 <div class="container">
-    <h6 class="subtitle">History Pengambilan Air <a href="/payriwayat"><br>lihat History pembayaran</a></h6>
+    <div class="row shadow p-3 rounded" style="background-color:red;">
+        <div class="col-12 px-0" style="margin: 7px auto 7px; width:100%">
+            <span class="material-icons" style="margin: 20px auto; display:inline;">
+                history
+            </span>
+            <h4 class="text-center" style=" display:inline;">History Pengambilan Air</h4>
+        </div>
+    </div>
+
 
     <div class="row">
         <div class="col-12 px-0">
@@ -13,9 +21,26 @@
                     <li class="list-group-item">
                         <div class="row align-items-center">
                             <div class="col-auto pr-0">
-                                <!-- <div class="avatar avatar-50 no-shadow border-0">
-                                <img src="img/user3.png" alt="">
-                            </div> -->
+                                <?php
+                                $a = substr($r['status'], 0, 1);
+                                if ($a == "R") : ?>
+                                    <span class="material-icons">
+                                        autorenew
+                                    </span>
+                                <?php elseif ($a == "V") : ?>
+                                    <span class="material-icons">
+                                        verified_user
+                                    </span>
+                                <?php elseif ($a == "T") : ?>
+                                    <span class="material-icons">
+                                        shopping_cart
+                                    </span>
+                                <?php else : ?>
+                                    <span class="material-icons">
+                                        android
+                                    </span>
+                                <?php endif; ?>
+
                             </div>
                             <div class="col align-self-center pr-0">
                                 <h6 class="font-weight-normal mb-1"><?= $r['status']; ?></h6>
@@ -24,7 +49,13 @@
                                 <p class="text-mute small text-secondary"><?= $r['created_at']; ?></p>
                             </div>
                             <div class="col-auto">
-                                <h6 class="text-success"><?= $r['isi']; ?> </h6>
+                                <?php
+                                $a = substr($r['status'], 0, 1);
+                                if ($a == "R") : ?>
+                                    <h6 class="text-danger"><?= $r['isi']; ?> </h6>
+                                <?php else : ?>
+                                    <h6 class="text-success"><?= $r['isi']; ?> </h6>
+                                <?php endif; ?>
                             </div>
                         </div>
                     </li>
